00f5d7c269326050bbff8f009e1a663ac73e876c
zjun <565061763@qq.com>
2025-03-03 09:19:46 +0800

    nullable state

--- lib/src/commonMain/kotlin/com/sd/lib/kmp/datetime/DateSelector.kt
+++ lib/src/commonMain/kotlin/com/sd/lib/kmp/datetime/DateSelector.kt
@@ -3,42 +3,37 @@ package com.sd.lib.kmp.datetime
 import kotlinx.datetime.LocalDate
 
 class DateSelector(
-  startDate: LocalDate = fDate(1900, 1, 1),
-  endDate: LocalDate = fCurrentDate(),
+  startDate: LocalDate,
+  endDate: LocalDate,
   private val onStateChanged: (State) -> Unit,
 ) {
   data class State(
-    val date: LocalDate?,
+    val date: LocalDate,
     val listYear: List<Int>,
     val listMonth: List<Int>,
     val listDayOfMonth: List<Int>,
     val indexOfYear: Int,
     val indexOfMonth: Int,
     val indexOfDayOfMonth: Int,
-  ) {
-    companion object {
-      val Empty = State(
-        date = null,
-        listYear = emptyList(),
-        listMonth = emptyList(),
-        listDayOfMonth = emptyList(),
-        indexOfYear = -1,
-        indexOfMonth = -1,
-        indexOfDayOfMonth = -1,
-      )
-    }
-  }
+  )
 
-  private val _startDate = startDate
-  private val _endDate = endDate.coerceAtLeast(startDate)
+  private val _startDate: LocalDate = startDate
+  private val _endDate: LocalDate = endDate.coerceAtLeast(startDate)
 
-  private var _state = State.Empty
+  private var _state: State? = null
 
-  val state: State get() = _state
+  val state: State? get() = _state
 
   fun setDate(date: LocalDate) {
     val safeDate = date.coerceIn(_startDate, _endDate)
+
     val oldState = _state
+    if (oldState == null) {
+      val initState = initState(safeDate).also { _state = it }
+      onStateChanged(initState)
+      return
+    }
+
     val newState = oldState.newState(safeDate).also { _state = it }
     if (newState != oldState) {
       onStateChanged(newState)
@@ -76,22 +71,50 @@ class DateSelector(
       indexOfDayOfMonth = date.dayOfMonth - listDayOfMonth.first(),
     )
   }
+
+  private fun initState(date: LocalDate): State {
+    val listYear = (_startDate.year.._endDate.year).toList()
+
+    val listMonth = run {
+      val end = if (date.year == _endDate.year) _endDate.monthNumber else 12
+      (1..end).toList()
+    }
+
+    val listDayOfMonth = run {
+      val end = if (date.year == _endDate.year && date.month == _endDate.month) {
+        _endDate.dayOfMonth
+      } else {
+        date.fMaxDayOfMonth()
+      }
+      (1..end).toList()
+    }
+
+    return State(
+      date = date,
+      listYear = listYear,
+      listMonth = listMonth,
+      listDayOfMonth = listDayOfMonth,
+      indexOfYear = date.year - listYear.first(),
+      indexOfMonth = date.monthNumber - listMonth.first(),
+      indexOfDayOfMonth = date.dayOfMonth - listDayOfMonth.first(),
+    )
+  }
 }
 
 fun DateSelector.selectYearWithIndex(index: Int) {
-  val date = state.date ?: return
+  val state = this.state ?: return
   val year = state.listYear.getOrNull(index) ?: return
-  setDate(date.fCopy(year = year))
+  setDate(state.date.fCopy(year = year))
 }
 
 fun DateSelector.selectMonthWithIndex(index: Int) {
-  val date = state.date ?: return
+  val state = this.state ?: return
   val month = state.listMonth.getOrNull(index) ?: return
-  setDate(date.fCopy(month = month))
+  setDate(state.date.fCopy(month = month))
 }
 
 fun DateSelector.selectDayOfMonthWithIndex(index: Int) {
-  val date = state.date ?: return
+  val state = this.state ?: return
   val dayOfMonth = state.listDayOfMonth.getOrNull(index) ?: return
-  setDate(date.fCopy(dayOfMonth = dayOfMonth))
+  setDate(state.date.fCopy(dayOfMonth = dayOfMonth))
 }
